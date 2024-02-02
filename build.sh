c102dd9ed206d7d16478a679b9c7d0c3de544426
Sergio Chica <sergio.chica@csic.es>
2024-02-02 16:52:39 +0100

    Added argument to skip compilation. Added arguments to run tests

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-set -x
-
 usage() {
     echo "Usage: $0 [--keep-ext][--sha2|sha3|hw|hw3]"
     echo "  --keep-ext: do not rebuild external libraries, i.e. mcl, gtest"
+    echo "  --skip: skip compilation"
     echo "  --debug: compile with debug symbols"
     echo "  --gtest: compile GTEST suite"
+    echo "  --test: run GTEST tests after compiliation"
+    echo "  --btest: run basic tests (GTEST not required) after compiliation"
     echo "  --sha2: compile using SHA2 in PS16 (SW). Default BLAKE"
     echo "  --sha3: compile using SHA3 in PS16/KTY04 (SW)"
     echo "  --hw/hw3: compile using SHA2/SHA3 (HW) in PS16/KTY04, requires PYNQ"
@@ -31,19 +32,27 @@ build() {
     [ -n "$HW3" ] && hw3="-DHW3=1"
     [[ "$(uname -a)" =~ (arm|aarch) ]] && comp="-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++"
     cd build && cmake $gtest $comp $debug $sha2 $sha3 $hw $hw3 .. \
-        && make $verbose
+        && make $verbose && cd -
 }
 
 for arg in "$@"; do
-    [[ "$arg" == -h || "$arg" == --help ]] && usage && exit
-    [[ "$arg" == --debug ]] && DEBUG=1
-    [[ "$arg" == --gtest ]] && GTEST=1
-    [[ "$arg" == --keep-ext ]] && KEEPEXT=1
-    [[ "$arg" == --sha2 ]] && SHA2=1
-    [[ "$arg" == --sha3 ]] && SHA3=1
-    [[ "$arg" == --hw ]] && HW=1
-    [[ "$arg" == --hw3 ]] && HW=1 && HW3=1
+    [[ "$arg" = -h || "$arg" = --help ]] && usage && exit
+    [ "$arg" = --skip ] && SKIP=1
+    [ "$arg" = --debug ] && DEBUG=1
+    [ "$arg" = --gtest ] && GTEST=1
+    [ "$arg" = --test ] && TEST=1
+    [ "$arg" = --btest ] && BTEST=1
+    [ "$arg" = --keep-ext ] && KEEPEXT=1
+    [ "$arg" = --sha2 ] && SHA2=1
+    [ "$arg" = --sha3 ] && SHA3=1
+    [ "$arg" = --hw ] && HW=1
+    [ "$arg" = --hw3 ] && HW=1 && HW3=1
 done
 
-[ -n "$KEEPEXT" ] && keep_ext || rebuild
-build
+if [ -z "$SKIP" ]; then
+    [ -n "$KEEPEXT" ] && keep_ext || rebuild
+    build
+fi
+
+[ -n "$TEST" ] && cd build && make test && cd -
+[ -n "$BTEST" ] && build/bin/demos ps16 && build/bin/demos kty04
